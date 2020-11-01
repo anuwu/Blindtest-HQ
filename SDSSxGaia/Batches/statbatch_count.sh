6f7302771c00de5edc4910d460f9eeb7f8db247d
Anwesh Bhattacharya <anwesh0304@gmail.com>
2020-11-02 01:31:34 +0530

    Added .png and .sh missed by .gitignore

---
+++ SDSSxGaia/Batches/statbatch_count.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# For the list of Batch directories in a batch list, output the sizes of the batches
+
+cat ../StatBatches/MNRAS/batches.txt | xargs find | grep -v "_result.csv$" | grep ".csv" | xargs wc -l
