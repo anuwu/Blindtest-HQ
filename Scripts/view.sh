d1993144ed33ac6cf0f4b87aba1217b3a6098fe5
Anwesh Bhattacharya <anwesh0304@gmail.com>
2020-11-07 04:00:01 +0530

    Created two different folders for view.sh - 'Keep ' and 'Del'

--- Scripts/view.sh
+++ Scripts/view.sh
@@ -3,15 +3,26 @@
 # keep or delete them. Enter for keep, 'd' for delete
 
 path="$1/*"
+
+if ! [[ test -e Keep/ ]]
+then
+        mkdir Keep/
+fi
+
+if ! [[ test -e Del/ ]]
+then
+        mkdir Del/
+fi
+
 for f in $path 
 do
-        # printf "$f\n"
         eog $f & pic_pid=$!
         read -p "d/Enter : " inp
         kill $pic_pid
         if [ "$inp" = "d" ]
         then
-                rm $f
+                mv $f Del/
+        else
+                mv $f Keep/
         fi
-        # echo $inp
 done 
