07d91e9804bc8163151c9313f5a615c83e64caad
Anwesh Bhattacharya <anwesh0304@gmail.com>
2020-11-06 19:19:49 +0530

    Gonna deal in batches of 1000 now

--- SDSSxGaia/Batches/statbatch_count.sh
+++ SDSSxGaia/Batches/statbatch_count.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-# For the list of Batch directories in a batch list, output the sizes of the batches
+# For the list of Batch directories in a batch list, outputs the total size of all the batches 
 
-cat ../StatBatches/$1/batches.txt | xargs find | grep -v "_result.csv$" | grep ".csv" | xargs wc -l
+lstat=$(cat ../StatBatches/$1/batches.txt | xargs find | grep -v "_result.csv$" | grep ".csv" | xargs wc -l)
+tot=$(printf "$lstat\n" | tail -1 | grep -o "[0-9]*")
+sub=$(printf "$lstat\n" | wc -l)
+sub=$[$sub-1]
+printf "$[$tot-$sub]\n"
