b2ef4034b4035513b305ddd7784d9f2a9c17a3d1
Anwesh Bhattacharya <anwesh0304@gmail.com>
2020-11-09 16:30:00 +0530

    Added SDSS Spectro

--- Scripts/impure_filter.sh
+++ Scripts/impure_filter.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Takes as input the impure_pids_temp.csv file and removes lines with no detected peaks
+# i.e. 3 commas consecutively
 
-cat $1 | grep -v "[0-9]*,,,"
-rm -i $1
+cat $1 | grep -v "[0-9]*,,,\|None"
